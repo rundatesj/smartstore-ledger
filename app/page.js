8daf0dc4b7ca78eeaded1604190df7580a9de142
rundatesj <bulnadda@gmail.com>
2026-04-09 20:13:23 +0900

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>스마트스토어 장부 자동화</h1>
+      <p>여기서 주문 / 정산 데이터를 조회할 예정입니다.</p>
+    </main>
+  );
+}
